312c114baf6cd46499ad817871258cb5689236db
JF Padilla <emailreel@gmail.com>
2018-02-28 20:37:38 +0000

    install devise, add authentication and update nav
    add devise gem, bundle and rails install
    update action mailer config for dev environment
    add devise user model
    update navigation

--- Gemfile
+++ Gemfile
@@ -71,5 +71,6 @@ end
 gem 'thor', '0.19.1'
 gem 'bootstrap-sass'
 gem 'figaro'
+gem 'devise'
 
 ruby "2.4.1"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +53,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.action_mailer.default_url_options = { host: $IP, port: $PORT }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
