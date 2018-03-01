22d45ebf1928b445044f6db958141818cf3d1437
JF Padilla <emailreel@gmail.com>
2018-03-01 01:12:54 +0000

    require login, add user identifier styling
    add authentication before action to application controller
    add styling to user email in navigation bar
    remove test mailer

--- app/mailers/support_mailer.rb
+++
@@ -1,7 +0,0 @@
-class SupportMailer < ApplicationMailer
-    default from: "emailreel@gmail.com"
-    
-    def test_mail
-        mail(to: "emailreel@gmail.com", subject: "Test EMail")
-    end
-end
--- spec/mailers/previews/support_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/support_mailer
-class SupportMailerPreview < ActionMailer::Preview
-
-end
