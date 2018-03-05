f0f844c4746b534ff5c5dc2c2023a18cc33801f5
JF Padilla <emailreel@gmail.com>
2018-03-05 16:39:08 +0000

    add wikis controller and define its index method
    rails generation wikis controller
    replace default route references with reference by resource keyword
    update route references in container html
    include devise test controller helper and update index specs
    define wiki index action
    update wiki index view

---
+++ spec/controllers/wikis_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe WikisController, type: :controller do
+  let(:my_user) { create(:user) }
+  let(:my_wiki) { create(:wiki, user: my_user) }
+
+  describe "GET #index" do
+    before do
+      sign_in my_user
+    end
+    
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "assigns [my_wiki] to @wikis" do
+      get :index
+      expect(assigns(:wikis)).to eq([my_wiki])
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     email RandomData.random_email
     password RandomData.random_word(8,16)
+    confirmed_at Time.now
   end
 end
