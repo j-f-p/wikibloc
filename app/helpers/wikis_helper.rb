708b77175893a92a88740140609ac91d6f39c57b
JF Padilla <emailreel@gmail.com>
2018-06-14 20:22:08 +0000

    hide private wikis and update seeds and some views
    in condition to display private, replace disjunction with method
    update wiki model spec to account for private attribute
    update wiki controller to have private updated from params
    add another premium member to db seeds, remove excess admin from seeds
    add private wikis to db seeds and update seeding summary
    show private wikis in wikis index view to authorized users only
    in wiki show view, show when last updated, and if private, the author
    label private wikis

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,11 @@
 module WikisHelper
+  def authorized_for_private_wikis?
+    current_user.admin? || current_user.premium?
+  end
+  
+  def wiki_viewable(wiki)
+    wiki.private!=true ||
+      current_user.admin? ||
+      ( current_user.premium? && current_user.id==wiki.user_id )
+  end
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Wiki, type: :model do
   let(:wiki) { create(:wiki) }
  
   describe "attributes" do
-    it "has title, body and user attributes" do
+    it "has title, body, private and user attributes" do
       expect(wiki).to have_attributes(title: wiki.title, body: wiki.body,
-        user: wiki.user)
+        private: wiki.private, user: wiki.user)
     end
   end
 end
