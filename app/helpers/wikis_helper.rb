b862e933902d3bfd602e2e7030741ee3ac93091d
JF Padilla <emailreel@gmail.com>
2018-06-20 15:08:00 +0000

    make public wikis unprivatizable

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,6 +1,6 @@
 module WikisHelper
-  def authorized_for_private_wikis?
-    current_user.admin? || current_user.premium?
+  def authorized_for_private_wikis?(wiki)
+    wiki.private? && (current_user.admin? || current_user.premium?)
   end
   
   def wiki_viewable(wiki)
