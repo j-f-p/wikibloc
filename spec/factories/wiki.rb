0a43431a05d8487b0ad02aa5e91da1b1c0498e09
JF Padilla <emailreel@gmail.com>
2018-07-10 00:39:12 +0000

    add basic validation for wiki upon creation
    update wiki model spec to account for basic validation
    add basic content validation in wiki model

--- spec/factories/wiki.rb
+++ spec/factories/wiki.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :wiki do
     title RandomData.random_sentence
     body RandomData.random_paragraph
+    private false
     user
   end
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  it { is_expected.to belong_to(:user) }
+  it { should belong_to(:user) }
+  
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  # it { should validate_inclusion_of(:private).in_array([true, false]) }
+    # works with warning to remove, thus removed. Validation works in app test.
+  it { should validate_presence_of(:user) }
+  
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(10) }
   
   let(:wiki) { create(:wiki) }
  
