b13e95ba339f82bd8805369c36e133a103ccf44a
JF Padilla <emailreel@gmail.com>
2018-08-01 22:31:23 +0000

    continue building collaborator new view (phase 3)
    pluralize headers of lists
    conditionally define form based on list 'plurality'
    format form field and correct display of lists
    reduce db calls and erb blocks
    add submit button
    begin building create action
    resolve passing of wiki_id and user_id to create action

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,6 +1,16 @@
 class CollaboratorsController < ApplicationController
+  # attr_accessor :new_cids # for 1 or more new collaborators
+  # attr_accessor :new_cids_wid
+  
   def new
     @collaborator=Collaborator.new
     @wiki=Wiki.find(params[:collab_wiki_id])
+  end
+  
+  def create
+    # adding 1 new collab
+    @collaborator = Collaborator.new
+    @collaborator.wiki_id = Integer(params[:new_cids_wid])
+    @collaborator.user_id = Integer(params[:new_cids])
   end
 end
